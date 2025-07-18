215bcfb0cf39744d5c355c10e093ff1c65852b6d
unknown <m.k.senary2001@gmail.com>
2025-07-18 23:33:12 +0300

    Modified Serializer,Added other Endpoints

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,23 +1,29 @@
 from rest_framework import serializers
-from ..models import Category , Post
+from ..models import Category , Post, Tag
 
 class CategorySerializer (serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
-class PostSerializer (serializers.ModelSerializer):
-    #POST,PUT,PATCH accept Category as id 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), allow_null=True)
-    
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True, required=False)
+
     class Meta:
         model = Post
         fields = '__all__'
-        read_only_fields = ['id' , 'created_at' , 'likes' , 'dislikes'] #likes and dislikes could later be removed 
-    
-    #GET representation
+        read_only_fields = ['id', 'likes', 'dislikes', 'author']
+
     def to_representation(self, instance):
-        rep =  super().to_representation(instance)
+        rep = super().to_representation(instance)
         rep['category'] = instance.category.name if instance.category else None
+        rep['tags'] = TagSerializer(instance.tags.all(), many=True).data
         return rep
+
 
